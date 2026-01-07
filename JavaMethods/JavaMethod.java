a1ef4d3e3e425005b1613615597334ec0c6a20ec
vijayshah263 <shahvijayrajendra02@gmail.com>
2026-01-07 16:01:31 +0530

    basics of java

---
+++ JavaMethods/JavaMethod.java
@@ -0,0 +1,12 @@
+package JavaMethods;
+
+public class JavaMethod {
+    public static void main(String[] args) {
+        myMethod();
+
+    }
+
+    public static void myMethod() {
+            System.out.println("Good Morning Vijay");
+    }
+}
